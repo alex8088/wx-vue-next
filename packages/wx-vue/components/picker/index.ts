676828327b43c4646ec2d2737c1d997e215a1b4f
alex8088 <244096523@qq.com>
2022-07-09 23:13:34 +0800

    perf(picker): export as default

--- packages/wx-vue/components/picker/index.ts
+++ packages/wx-vue/components/picker/index.ts
@@ -4,6 +4,8 @@ import Picker from './picker.vue'
 
 export const WxPicker: WithInstallType<typeof Picker> = withInstall(Picker)
 
+export default WxPicker
+
 export type WxPickerInstance = InstanceType<typeof Picker>
 
 export type { PickerProps, PickerOption, PickerOptions } from './types'
